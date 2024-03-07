ec764ab3f2703485beddfc9e5cd6e6effec6d547
Your Name <you@example.com>
2024-03-07 09:46:09 -0800

    Work In Progress

--- src/Sum.java
+++ src/Sum.java
@@ -7,20 +7,37 @@ public class Sum {
     private static final String DATA = "src/sum.txt";
     private static final int ARRAY_SIZE = 25;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(DATA));
+        processFile(input);
     }
 
-    private static void processFile() {
+    private static void processFile(Scanner input) {
 
+        int lineCount = 0;
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            Scanner lineScanner = new Scanner(line);
+
+            while (lineScanner.hasNext()) {
+                String token = lineScanner.next();
+            }
+
+        }
     }
 
     private static int[] stringToDigitArray() {
 
+        for (int i = ARRAY_SIZE - 1; i >= 0; i--) {
+
+        }
     }
 
     private static int addLargeNumbers() {
 
+        int carry = sum % 10;
+        return carry;
     }
 
     private static void printSum() {
